060e41f6a80d39ef23ad7495ba6da9f3b855388d
kwangmo <ahnmo777@gmail.com>
2018-06-01 11:19:22 +0900

    세일즈팜 모델 ver 0.1.11

--- types/user-image.d.ts
+++ types/user-image.d.ts
@@ -1,7 +1,7 @@
 import {IImage} from './image';
 
 export interface IUserImage extends IImage {
-  readonly small?: string;
-  readonly medium?: string;
-  readonly large?: string;
+
+  // foreign keys
+  userId?: number;
 }
