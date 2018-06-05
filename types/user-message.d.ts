561a8b2f92dca0cb4abd27aa4aacb3c24abe30f1
kwangmo <ahnmo777@gmail.com>
2018-06-05 14:13:55 +0900

    세일즈팜 모델 ver 0.1.25  - UserMessage && SalespalmMessage 추가

--- types/user-message.d.ts
+++ types/user-message.d.ts
@@ -12,7 +12,6 @@ export interface IUserMessage {
 
 export interface IRecommendeeMessageData extends IMessageData {
   recommendee: number;  // 추천한 사람의 아이디
-  recommender: number;         // 추천 받은 사람의 아이디
 }
 
 export interface IRecommendeeMessage extends IUserMessage {
