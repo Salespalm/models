6b5d5e402f3be73672de3eec363a95117c3d2238
kwangmo <ahnmo777@gmail.com>
2018-06-18 13:15:53 +0900

    세일즈팜 모델 ver 0.1.31  - ICompany 필드를 모두 옵션 사항으로 변경, update 요청에서 부분적인 업데이트에 사용하기 위함

--- types/file.d.ts
+++ types/file.d.ts
@@ -3,6 +3,6 @@ export interface IFile {
   createdAt?: Date;  // 파일 생성일
 
   filename?: string;  // 파일 이름
-  mimeType?: string;  // 파일의 mime type
+  mimetype?: string;  // 파일의 mime type
   bucket?: string;    // 파일이 등록된 s3 스토리지의 버킷
 }
