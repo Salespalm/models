28053d16deb98b0547c703aa44244e865b26311f
kwangmo <ahnmo777@gmail.com>
2018-07-02 00:37:13 +0900

    세일즈팜 모델 ver 0.1.40  - ISimpleResponse 모델에 success 이외에 다른 properties를 추가할 수 있도록 수정

--- types/simple-response.d.ts
+++ types/simple-response.d.ts
@@ -1,7 +1,6 @@
 import {IModel} from './model';
-import {ILanguage} from './language';
 
 export interface ISimpleResponse extends IModel {
   success: boolean;
-  message?: ILanguage | string;
+  [prop: string]: any;
 }
