ef823601ce013e948c421314da318652eb6c7a43
kwangmo <ahnmo777@gmail.com>
2018-07-18 17:30:17 +0900

    세일즈팜 모델 ver 0.1.46  - ISalesItemSalesItemRegion, ISalesItemSalesItemTag 모델에 sequence 필드 추가

--- types/sales-item-sales-item-region.d.ts
+++ types/sales-item-sales-item-region.d.ts
@@ -2,4 +2,5 @@ export interface ISalesItemSalesItemRegion {
   // 상품과 영업 지역을 연결하기 위한 모델
   readonly itemId: number;    // 상품의 아이디
   readonly regionId: number;  // 지역의 아이디
+  sequence?: number;
 }
--- types/sales-item-sales-item-tag.d.ts
+++ types/sales-item-sales-item-tag.d.ts
@@ -2,4 +2,5 @@ export interface ISalesItemSalesItemTag {
   // 상품과 태그를 이어주기 위한 모델
   itemId: number;
   tagId: string;
+  sequence?: number;
 }
