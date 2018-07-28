b0c6d15b52dbf5a70ddf3bb11246f628c8c63213
kwangmo <ahnmo777@gmail.com>
2018-07-28 21:30:19 +0900

    세일즈팜 모델 ver 0.1.47  - Message 및 Push message 관련 모델 수정

--- types/message.d.ts
+++ types/message.d.ts
@@ -8,11 +8,32 @@ export declare type THomepageLink = '';
 export declare type TAdminAppLink = '';
 export declare type TCompanyAppLink = '';
 
+export interface IAppLink {
+  link: string;
+  data?: any;
+}
+
+export interface IMobileAppLink extends IAppLink {
+  link: TMobileAppLink;
+}
+
+export interface IHomepageLink extends IAppLink {
+  link: THomepageLink;
+}
+
+export interface ICompanyAppLink extends IAppLink {
+  link: TCompanyAppLink;
+}
+
+export interface IAdminAppLink extends IAppLink {
+  link: TAdminAppLink;
+}
+
 export interface IMessageAppLink {
-  mobile?: TMobileAppLink;
-  homepage?: THomepageLink;
-  admin?: TAdminAppLink;
-  company?: TCompanyAppLink;
+  mobile?: IMobileAppLink;
+  homepage?: IHomepageLink;
+  company?: ICompanyAppLink;
+  admin?: IAdminAppLink;
 }
 
 export interface IMessageData {
--- types/user-message.d.ts
+++ types/user-message.d.ts
@@ -11,10 +11,10 @@ export interface IUserMessage {
   userId?: number;
 }
 
-export interface IRecommendeeMessageData extends IMessageData {
-  recommendee: number;  // 추천한 사람의 아이디
-}
-
-export interface IRecommendeeMessage extends IUserMessage {
-  data: IRecommendeeMessageData;
-}
+// export interface IRecommendeeMessageData extends IMessageData {
+//   recommendee: number;  // 추천한 사람의 아이디
+// }
+//
+// export interface IRecommendeeMessage extends IUserMessage {
+//   data: IRecommendeeMessageData;
+// }
