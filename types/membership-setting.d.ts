934bb06506c6b5f69bd264f51db2ebb92c4d3f27
kwangmo <ahnmo777@gmail.com>
2018-08-12 22:22:06 +0900

    세일즈팜 모델 ver 0.1.64  - IMembershipSettingItem 인터페이스의 price 필드의 데이터 타입을 string 에서 number 로 변경

--- types/membership-setting.d.ts
+++ types/membership-setting.d.ts
@@ -4,7 +4,7 @@ export interface IMembershipSettingItem {
   readonly id?: number;
 
   name: TMembership;          // 회원권 이름
-  price: string;              // 회원권 가격
+  price: number;              // 회원권 가격
   recommenderMileage: number; // 추천인에게 지급되는 마일리지
   period: number;             // 회원권 기간
   dueDay: number;             // 결제 기한(결제 요청일로부터의 일 수)
