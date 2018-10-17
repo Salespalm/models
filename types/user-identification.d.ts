87baeb3247665e72cd16ce875545ca226c9b2967
kwangmo <ahnmo777@gmail.com>
2018-10-17 13:39:29 +0900

    세일즈팜 모델 ver 0.2.10   - IUserIdentification 인터페이스에서 status 필드 제거

--- types/user-identification.d.ts
+++ types/user-identification.d.ts
@@ -1,9 +1,13 @@
 import { IUrlLike } from './file';
 import { IUser } from './user';
 
+export declare type TUserIdentificationStatus = 'wait' | 'reject' | 'confirm';
+
 export interface IUserIdentification extends IUrlLike {
   createdAt?: Date;
   updatedAt?: Date;
+
+  status?: TUserIdentificationStatus;
 
   // foreign keys
   userId?: number;
