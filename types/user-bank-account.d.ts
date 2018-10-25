f049603bf4be2be72334d954cfb9be0bf14f9f34
kwangmo <ahnmo777@gmail.com>
2018-10-25 20:12:58 +0900

    세일즈팜 모델 ver 0.2.15   - IUserBankAccount에 status 필드 추가

--- types/user-bank-account.d.ts
+++ types/user-bank-account.d.ts
@@ -1,7 +1,12 @@
 import { IModel } from './model';
 import { IBankAccount } from './bank-account';
 
+export declare type TUserBankAccountStatus = 'wait' | 'confirm' | 'reject';
+
 export interface IUserBankAccount extends IModel, IBankAccount {
+
+  status?: TUserBankAccountStatus;
+
   // foreign keys
   userId?: number;
 }
