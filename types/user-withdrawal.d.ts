1554e34e526e6fe4640438b223ba0c3eefb52d9c
kwangmo <ahnmo777@gmail.com>
2018-10-26 15:22:23 +0900

    세일즈팜 모델 ver 0.2.21   - IUserWithdrawal 인터페이스에 withdrawnDate 필드 추가

--- types/user-withdrawal.d.ts
+++ types/user-withdrawal.d.ts
@@ -5,11 +5,11 @@ export declare type TUserWithdrawalStatus = 'wait' | 'reject' | 'confirm';
 export interface IUserWithdrawal {
   id?: string;
   createdAt?: Date;
+  withdrawnDate?: Date;
 
   point: number;
   transferFee: number;
   status?: TUserWithdrawalStatus;
-
 
   // foreign key
   userId?: number;
